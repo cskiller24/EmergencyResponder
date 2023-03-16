b065d30e98dff405868b51e6c87a314ba8dd91f4
DCCadayong <78188741+cskiller24@users.noreply.github.com>
2023-03-17 02:22:36 +0800

    Added str helpers

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,7 @@
 
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Str;
 
 if (! function_exists('redirectRole')) {
     function redirectRole(User $user): string
@@ -26,5 +27,25 @@ if (! function_exists('route_named')) {
     function route_named(string $path): bool
     {
         return request()->route()->named($path);
+    }
+}
+
+if (! function_exists('str_title')) {
+    /**
+     * Turn snake case into title case
+     */
+    function str_title(string $string): string
+    {
+        return Str::title(str_replace('_', ' ', $string));
+    }
+}
+
+if (! function_exists('str_snake')) {
+    /**
+     * Turn string into snake case
+     */
+    function str_snake(string $string): string
+    {
+        return Str::snake($string);
     }
 }
