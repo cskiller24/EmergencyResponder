a39a3fac5cb2d334678ad0099558852db3d2dc1f
DCCadayong <78188741+cskiller24@users.noreply.github.com>
2023-04-07 22:48:22 +0800

    Remove unused methods

--- app/Http/Controllers/Web/Admin/UserController.php
+++ app/Http/Controllers/Web/Admin/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Web\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class UserController extends Controller
@@ -24,14 +23,6 @@ class UserController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(User $user): View
@@ -41,29 +32,5 @@ class UserController extends Controller
         $roles = $user->getRoleNames();
 
         return view('admin.users-show', compact('user', 'roles'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        $this->authorize('user_show', User::class);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $this->authorize('update', User::class);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
