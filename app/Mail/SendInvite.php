9c828fa068684758b521a4b2f539d65b38c7a37b
DCCadayong <78188741+cskiller24@users.noreply.github.com>
2023-04-14 22:46:28 +0800

    Queue invitation mails

--- app/Mail/SendInvite.php
+++ app/Mail/SendInvite.php
@@ -4,12 +4,13 @@ namespace App\Mail;
 
 use App\Models\Invite;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendInvite extends Mailable
+class SendInvite extends Mailable implements ShouldQueue
 {
     use Queueable;
     use SerializesModels;
